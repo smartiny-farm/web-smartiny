ad3cf30c9056209f18717a87dd2ab2417077ae66
Milena <milenaalmeidadev@hotmail.com>
2021-08-22 03:48:45 -0400

    refactor: cameras working

--- src/pages/Camera.js
+++ src/pages/Camera.js
@@ -14,12 +14,61 @@ export default function Camera() {
       <Grid container spacing={3}>
         <Grid item xs={16} md={10} lg={6}>
           <Card>
-            <CardHeader title="Câmera 01" />
+            <CardHeader title="Galpão de máquinas" />
+            <iframe
+              title="cam01"
+              width="560"
+              height="415"
+              src="http://192.168.1.115"
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
           </Card>
         </Grid>
         <Grid item xs={16} md={10} lg={6}>
           <Card>
             <CardHeader title="Câmera 02" />
+            <iframe
+              title="cam02"
+              width="560"
+              height="415"
+              src="http://192.168.1.116"
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </Card>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={3} marginTop={3}>
+        <Grid item xs={16} md={10} lg={6}>
+          <Card>
+            <CardHeader title="Câmera 03" />
+            <iframe
+              title="cam03"
+              width="560"
+              height="415"
+              src="http://192.168.1.117"
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </Card>
+        </Grid>
+        <Grid item xs={16} md={10} lg={6}>
+          <Card>
+            <CardHeader title="Câmera 04" />
+            <iframe
+              title="cam04"
+              width="560"
+              height="415"
+              src="http://192.168.1.118"
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
           </Card>
         </Grid>
       </Grid>
